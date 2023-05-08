3e041df9a12f149c600a8fd924b5f7a2e5d713e9
Pierre <pierrefraisse90@gmail.com>
2023-05-08 15:03:03 +0200

    styling

--- src/components/tabs/Tabs.js
+++ src/components/tabs/Tabs.js
@@ -3,28 +3,31 @@ import './Tabs.scss'
 
 const Tabs = () => {
   return (
-    <section className='tabs-section'>
+    <section className='tabs-section --100vh --flex-center'>
         <div className="container">
-
-            <div className="tabs-heading">
-                <h2>More About Us</h2>
-                <p>Click the button to learn more</p>
+            <div className="tabs-heading --text-center">
+                <h2 className='--line --p'>More About Us</h2>
+                <p className='--text-sm'>Click the button to learn more</p>
             </div>
-
-            <div className="tabs">
+            <div className="tabs --flex-start --bg-light">
                 <div className="tabs-title">
-                    <button className='tab'>Who We Are</button>
+                    <button className='tab active-tab'>Who We Are</button>
                     <button className='tab'>What We Do</button>
                     <button className='tab'>Get In Touch</button>
                 </div>
                 <div className="tabs-content">
+                    <article className='content active-content'>
+                    <h4>Who We Are...</h4>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde repellat sapiente doloribus quasi quisquam doloremque voluptates, pariatur iure ex voluptatum eos omnis reprehenderit neque libero nemo consequuntur debitis ipsum atque.</p>
                     <article className='content'>
-                        <h4>Who We Are...</h4>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde repellat sapiente doloribus quasi quisquam doloremque voluptates, pariatur iure ex voluptatum eos omnis reprehenderit neque libero nemo consequuntur debitis ipsum atque.</p>
                         <h4>What We Do...</h4>
                         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde repellat sapiente doloribus quasi quisquam doloremque voluptates, pariatur iure ex voluptatum eos omnis reprehenderit neque libero nemo consequuntur debitis ipsum atque.</p>
+                    </article>
+                    <article className='content'>
                         <h4>Get In Touch...</h4>
                         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde repellat sapiente doloribus quasi quisquam doloremque voluptates, pariatur iure ex voluptatum eos omnis reprehenderit neque libero nemo consequuntur debitis ipsum atque.</p>
+                    </article>
+                        
                     </article>
                 </div>
             </div>
